e43ed99ed5d868a08546e50ce774ec855bc7fb03
PxOliver <zaulx300@gmail.com>
2025-12-03 21:07:49 -0500

    Cambio

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,11 +1,9 @@
-// src/api/axios.ts
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
 });
 
-// Agrega el token a cada request si existe
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem('authToken');
   if (token) {
